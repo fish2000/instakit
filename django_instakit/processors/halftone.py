d7f3d2cf3d7c5daef1141f531f788d065ffa5d28
Alexander Böhn <fish2000@gmail.com>
2012-12-29 15:43:15 -0500

    First version of CMYK Dot Screen halftone processor has been ported and is somewhat operational!!!

--- django_instakit/processors/halftone.py
+++ django_instakit/processors/halftone.py
@@ -6,8 +6,13 @@ halftone.py
 Created by FI$H 2000 on 2012-08-23.
 Copyright (c) 2012 Objects In Space And Time, LLC. All rights reserved.
 """
-import numpy
+
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageStat
+
+from django_instakit.utils import pipeline
+from django_instakit.utils.gcr import gcr
 
 class Atkinson(object):
     
@@ -29,48 +34,60 @@ class Atkinson(object):
                         pass # it happens, evidently.
         return img
 
-class NumAtkinson(object):
+
+class DotScreen(object):
     
-    threshold = 128.0
-    threshold_matrix = int(threshold)*[0] + (256-int(threshold))*[255]
-    
-    def _error(self, x, y, thresh=None):
-        error_val = (int(self.omtx[x, y]) - thresh) >> 3
-        try:
-            self.omtx[x+1, y] += error_val
-        except:
-            pass
-        try:
-            self.omtx[x+2, y] += error_val
-        except:
-            pass
-        try:
-            self.omtx[x-1, y+1] += error_val
-        except:
-            pass
-        try:
-            self.omtx[x, y+1] += error_val
-        except:
-            pass
-        try:
-            self.omtx[x+1, y+1] += error_val
-        except:
-            pass
-        try:
-            self.omtx[x, y+2] += error_val
-        except:
-            pass
+    def __init__(self, sample=1, scale=2, angle=0):
+        self.sample = sample
+        self.scale = scale
+        self.angle = angle
     
     def process(self, img):
-        img = img.convert('L')
-        imtx = numpy.array(img)
-        self.omtx = imtx
-        for y in xrange(imtx.shape[1]):
-            for x in xrange(imtx.shape[0]):
-                thresh = self.threshold_matrix[self.omtx[x, y]]
-                self._error(x, y, thresh=thresh)
-                self.omtx[x, y] = thresh
-        return Image.fromarray(self.omtx, mode='L').convert('RGB')
+        origsize = img.size
+        img = img.convert('L').rotate(self.angle, expand=1)
+        size = img.size[0]*self.scale, img.size[1]*self.scale
+        halftone = Image.new('L', size)
+        dotscreen = ImageDraw.Draw(halftone)
+        
+        for x in xrange(0, img.size[0], self.sample):
+            for y in xrange(0, img.size[0], self.sample):
+                cropbox = img.crop(
+                    (x, y, x+self.sample, y+self.sample))
+                stat = ImageStat.Stat(cropbox)
+                diameter = (stat.mean[0] / 255) ** 0.5
+                edge = 0.5 * (1-diameter)
+                xpos, ypos = (x+edge)*self.scale, (y+edge)*self.scale
+                boxedge = self.sample * diameter * self.scale
+                dotscreen.ellipse(
+                    (xpos, ypos, xpos+boxedge, ypos+boxedge),
+                    fill=255)
+        
+        halftone = halftone.rotate(-self.angle, expand=1)
+        halfwidth, halfheight = halftone.size
+        xx = (halfwidth - origsize[0]*self.scale) / 2
+        yy = (halfheight - origsize[1]*self.scale) / 2
+        return halftone.crop(
+            (xx, yy, xx+origsize[0]*self.scale, yy+origsize[1]*self.scale))
+
+
+class CMYKDotScreen(object):
+    
+    def __init__(self,
+        gcr=0, sample=10, scale=1,
+        thetaC=0, thetaM=15, thetaY=30, thetaK=45):
+        
+        self.gcr = max(min(100, gcr), 0)
+        self.overprinter = pipeline.ChannelFork(DotScreen, mode='CMYK')
+        self.overprinter.update({
+            'C': DotScreen(angle=thetaC, sample=sample, scale=scale),
+            'M': DotScreen(angle=thetaM, sample=sample, scale=scale),
+            'Y': DotScreen(angle=thetaY, sample=sample, scale=scale),
+            'K': DotScreen(angle=thetaK, sample=sample, scale=scale), })
+    
+    def process(self, img):
+        return self.overprinter.process(
+            gcr(img, self.gcr))
+
 
 
 if __name__ == '__main__':
@@ -85,8 +102,8 @@ if __name__ == '__main__':
     
     for image_input in image_inputs[:2]:
         #image_input.show()
-        Atkinson().process(image_input).show()
-        NumAtkinson().process(image_input).show()
+        #Atkinson().process(image_input).show()
+        CMYKDotScreen(sample=6, scale=2).process(image_input).show()
     
     print image_paths
     
---
+++ django_instakit/utils/gcr.py
@@ -0,0 +1,27 @@
+
+from PIL import Image
+
+def gcr(im, percentage):
+    ''' basic "Gray Component Replacement" function. Returns a CMYK image with 
+        percentage gray component removed from the CMY channels and put in the
+        K channel, ie. for percentage=100, (41, 100, 255, 0) >> (0, 59, 214, 41) '''
+    # from http://stackoverflow.com/questions/10572274/halftone-images-in-python
+    
+    cmyk_im = im.convert('CMYK')
+    if not percentage:
+        return cmyk_im
+    
+    cmyk_im = cmyk_im.split()
+    cmyk = []
+    
+    for i in xrange(4):
+        cmyk.append(cmyk_im[i].load())
+    
+    for x in xrange(im.size[0]):
+        for y in xrange(im.size[1]):
+            gray = min(cmyk[0][x,y], cmyk[1][x,y], cmyk[2][x,y]) * percentage / 100
+            for i in xrange(3):
+                cmyk[i][x,y] = cmyk[i][x,y] - gray
+            cmyk[3][x,y] = gray
+    
+    return Image.merge('CMYK', cmyk_im)
