38a8b39b4dc23836a0dc7ed169975a3d0c7e98a1
Alexander Böhn <fish2000@gmail.com>
2014-06-20 06:34:27 -0700

    Tons of new PIL/Pillow-based processors in adjust.py and blur.py

--- instakit/processors/adjust.py
+++ instakit/processors/adjust.py
@@ -1,5 +1,5 @@
 
-from PIL import ImageOps
+from PIL import ImageOps, ImageChops
 from PIL.ImageEnhance import Color as _Color
 from PIL.ImageEnhance import Brightness as _Brightness
 from PIL.ImageEnhance import Contrast as _Contrast
@@ -17,23 +17,31 @@ class Adjustment(object):
         return img
 
 class Color(Adjustment):
+    """ Globally tweak the image color """
     def adjust(self, img):
         return _Color(img).enhance(self.value)
 
 class Brightness(Adjustment):
+    """ Adjust the image brightness """
     def adjust(self, img):
         return _Brightness(img).enhance(self.value)
 
 class Contrast(Adjustment):
+    """ Adjust the image contrast """
     def adjust(self, img):
         return _Contrast(img).enhance(self.value)
 
 class Sharpness(Adjustment):
+    """ Adjust the sharpness of the image """
     def adjust(self, img):
         return _Sharpness(img).enhance(self.value)
 
+class Invert(object):
+    def process(self, img):
+        return ImageChops.invert(img)
 
 class Equalize(object):
+    """ Apply a non-linear mapping to the image, via histogram """
     def __init__(self, mask=None):
         self.mask = hasattr(mask, 'copy') and \
             mask.copy() or mask
@@ -41,8 +49,8 @@ class Equalize(object):
         return ImageOps.equalize(img,
             mask=self.mask)
 
-
 class AutoContrast(object):
+    """ Normalize contrast throughout the image, via histogram """
     def __init__(self, cutoff=0, ignore=None):
         self.cutoff = cutoff
         self.ignore = ignore
@@ -50,3 +58,20 @@ class AutoContrast(object):
         return ImageOps.autocontrast(img,
             cutoff=self.cutoff,
             ignore=self.ignore)
+
+class Solarize(object):
+    """ Invert all pixel values above an 8-bit threshold """
+    def __init__(self, threshold=128):
+        self.threshold = min(max(1, threshold), 255)
+    def process(self, img):
+        return ImageOps.solarize(img,
+            threshold=self.threshold)
+
+class Posterize(object):
+    """ Reduce the number of bits (1 to 8) per channel """
+    def __init__(self, bits=4):
+        self.bits = min(max(1, bits), 8)
+    def process(self, img):
+        return ImageOps.posterize(img,
+            bits=self.bits)
+    
--- instakit/processors/blur.py
+++ instakit/processors/blur.py
@@ -8,9 +8,83 @@ Copyright (c) 2012 Objects In Space And Time, LLC. All rights reserved.
 """
 from instakit.utils import kernels
 
+from PIL.ImageFilter import UnsharpMask as PILUnsharpMask
+from PIL.ImageFilter import GaussianBlur as PILGaussianBlur
+
+from PIL.ImageFilter import CONTOUR, DETAIL, EMBOSS
+from PIL.ImageFilter import EDGE_ENHANCE, EDGE_ENHANCE_MORE, FIND_EDGES
+from PIL.ImageFilter import SMOOTH, SMOOTH_MORE, SHARPEN
+
+
+class ImagingCoreFilterMixin(object):
+    def process(self, img):
+        return img.filter(self)
+
+
+class Contour(CONTOUR, ImagingCoreFilterMixin):
+    """ Contour-Enhance Filter """
+    pass
+
+
+class Detail(DETAIL, ImagingCoreFilterMixin):
+    """ Detail-Enhance Filter """
+    pass
+
+
+class Emboss(EMBOSS, ImagingCoreFilterMixin):
+    """ Emboss-Effect Filter """
+    pass
+
+
+class EdgeEnhance(EDGE_ENHANCE, ImagingCoreFilterMixin):
+    """ Edge-Enhance Filter """
+    pass
+
+
+class EdgeEnhanceMore(EDGE_ENHANCE_MORE, ImagingCoreFilterMixin):
+    """ Edge-Enhance (With Extreme Predjudice) Filter """
+    pass
+
+
+class FindEdges(FIND_EDGES, ImagingCoreFilterMixin):
+    """ Edge-Finder Filter """
+    pass
+
+
+class Smooth(SMOOTH, ImagingCoreFilterMixin):
+    """ Image-Smoothing Filter """
+    pass
+
+
+class SmoothMore(SMOOTH_MORE, ImagingCoreFilterMixin):
+    """ Image-Smoothing (With Extreme Prejudice) Filter """
+    pass
+
+
+class Sharpen(SHARPEN, ImagingCoreFilterMixin):
+    """ Image Sharpener """
+    pass
+
+
+class UnsharpMask(PILUnsharpMask, ImagingCoreFilterMixin):
+    """ Unsharp Mask Filter 
+        Optionally initialize with params:
+            radius (2), percent (150), threshold (3) """
+    pass
+
+
+class SimpleGaussianBlur(PILGaussianBlur, ImagingCoreFilterMixin):
+    """ Simple Gaussian Blur Filter 
+        Optionally initialize with radius (2) """
+    pass
+
+
 class GaussianBlur(object):
-    """ Gaussian Blur """
-    
+    """ Gaussian Blur Filter 
+        Optionally initialize with params:
+            n [aka sigma] (3)
+            nY (same as n)
+    """
     def __init__(self, n=3, nY=None):
         self.n = n
         if nY is not None:
@@ -26,6 +100,7 @@ class GaussianBlur(object):
             sigma=self.n)
         return Image.fromarray(out)
 
+
 if __name__ == '__main__':
     from PIL import Image
     from instakit.utils import static
@@ -38,8 +113,20 @@ if __name__ == '__main__':
             image_paths)
     
     for image_input in image_inputs:
-        image_input.show()
+        #image_input.show()
+        #GaussianBlur(n=3).process(image_input).show()
+        Contour().process(image_input).show()
+        Detail().process(image_input).show()
+        Emboss().process(image_input).show()
+        EdgeEnhance().process(image_input).show()
+        EdgeEnhanceMore().process(image_input).show()
+        FindEdges().process(image_input).show()
+        Smooth().process(image_input).show()
+        SmoothMore().process(image_input).show()
+        Sharpen().process(image_input).show()
+        UnsharpMask().process(image_input).show()
         GaussianBlur(n=3).process(image_input).show()
+        SimpleGaussianBlur(radius=3).process(image_input).show()
     
     print image_paths
     
