a5963004b6668fdddfc142aa1ef92fec92293c38
Alexander Böhn <fish2000@users.noreply.github.com>
2018-01-23 18:09:20 -0500

    Let `instakit.utils.gcr(…) return non-CMYK images
    … Added a `revert_mode` boolean keyword argument, defaulting to `False`, to accomplish this
    … Also tidied up and generally OCD’d the function a little bit in all sorts of gratuitous ways

--- instakit/utils/gcr.py
+++ instakit/utils/gcr.py
@@ -1,28 +1,38 @@
 
 from PIL import Image
 
-def gcr(im, percentage):
-    ''' basic "Gray Component Replacement" function. Returns a CMYK image with 
+def gcr(image, percentage, revert_mode=False):
+    ''' basic "Gray Component Replacement" function. Returns a CMYK image* with 
         percentage gray component removed from the CMY channels and put in the
-        K channel, ie. for percentage=100, (41, 100, 255, 0) >> (0, 59, 214, 41)
+        K channel, ie. for percentage=100, (41, 100, 255, 0) >> (0, 59, 214, 41).
+        
+    {*} This is the default behavior – to return an image of the same mode as that
+        of which was originally provided, pass the value for the (optional) keyword
+        argument `revert_mode` as `True`.
     '''
     # from http://stackoverflow.com/questions/10572274/halftone-images-in-python
     
-    cmyk_im = im.convert('CMYK')
     if not percentage:
-        return cmyk_im
+        return revert_mode and image or image.convert('CMYK')
     
-    cmyk_im = cmyk_im.split()
+    original_mode = image.mode
+    cmyk_image = image.mode is 'CMYK' and image.split() or image.convert('CMYK').split()
+    
     cmyk = []
+    for idx in xrange(4):
+        cmyk.append(cmyk_image[idx].load())
     
-    for i in xrange(4):
-        cmyk.append(cmyk_im[i].load())
+    for x in xrange(image.size[0]):
+        for y in xrange(image.size[1]):
+            gray = min(cmyk[0][x, y],
+                       cmyk[1][x, y],
+                       cmyk[2][x, y]) * percentage / 100
+            for idx in xrange(3):
+                cmyk[idx][x, y] -= gray
+            cmyk[3][x, y] = gray
     
-    for x in xrange(im.size[0]):
-        for y in xrange(im.size[1]):
-            gray = min(cmyk[0][x,y], cmyk[1][x,y], cmyk[2][x,y]) * percentage / 100
-            for i in xrange(3):
-                cmyk[i][x,y] = cmyk[i][x,y] - gray
-            cmyk[3][x,y] = gray
+    out = Image.merge('CMYK', cmyk_image)
+    if revert_mode:
+        out = out.convert(original_mode)
     
-    return Image.merge('CMYK', cmyk_im)
+    return out
