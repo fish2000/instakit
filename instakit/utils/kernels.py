3c31b16e6057d6936c695054c058a2d5757a80ea
Alexander Böhn <fish2000@gmail.com>
2018-12-27 16:02:12 -0500

    A proper Python-file prologue for instakit.utils.kernels

--- instakit/utils/kernels.py
+++ instakit/utils/kernels.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from __future__ import print_function
 
 def gaussian(size, sizeY=None):
     """ Returns a normalized 2D gauss kernel array for convolutions """
