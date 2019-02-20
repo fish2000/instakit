5c9cc4731b3496d2787e65256a583733e399ef17
Alexander Böhn <fish2000@gmail.com>
2019-02-20 10:15:43 -0500

    Optimized and expanded instakit.utils.stats ... added “color_count(image)” which gives you W * H * band count ... optimized the fuck out of “histogram_sum(image)” ... also optimized “histogram_entropy_py(image)” -- for some reason     all this histogram-manipulation Python PIL code floating around     which I and many others have adapted has always been doing like     a `sum(histogram)` type of thing to get the same fucking number     that you can get much faster just with `width * height * bands`     -- q.v. new “color_count(…)” func, supra. -- I assume that at     some point in the past, some C-hacker adapted some code that     only had a histogram value array to work with, and so had no     other option except to sum the values without having the image     dimensional data to work with… and but that adaptation carried     over that operation unnecessarily into the PIL-verse. And but     everyone who came along in the wake of this C-hacking imaging-     code-porting Children Of The Forest type did not really venture     into the nuts-‘n’-bolts of it and just copy-pasted what worked     with whatever minor jiggling was necessary for the way to work     it, no diggity, in order to bag it up. And it all stayed so     sub-optimally sub-optimal… UNTIL TODAY, when I nerded out on     it because it’s snowy as fuck in Bmore, dogg, yeah. ... Made sure the docstring for “histogram_sum(…)” explains what it     does in detail, as I replaced what was legible self-explanatory     code with a crafty yet opaque one-liner.

--- instakit/utils/stats.py
+++ instakit/utils/stats.py
@@ -7,26 +7,39 @@ Created by FI$H 2000 on 2018-12-24.
 Copyright (c) 2018 Objects In Space And Time, LLC. All rights reserved.
 """
 from __future__ import print_function
-
 from instakit.utils.mode import Mode
-from PIL import Image
 
 def pixel_count(image):
     """ Return the number of pixels in the input image. """
     width, height = image.size
     return width * height
 
+def color_count(image):
+    """ Return the number of color values in the input image --
+        this is the number of pixels times the band count
+        of the image.
+    """
+    width, height = image.size
+    return width * height * Mode.of(image).band_count
+
 def histogram_sum(image):
-    """ Return the sum of the input images’ histogram values. """
+    """ Return the sum of the input images’ histogram values --
+        Basically this is an optimized way of doing:
+            
+            out = 0.0
+            for value, count in enumerate(histogram):
+                out += value * count
+            return out
+            
+        … the one-liner uses the much faster sum(…) in léu
+        of looping over the histogram’s enumerated values.
+    """
     histogram = Mode.L.process(image).histogram()
-    out = 0.0
-    for idx, val in enumerate(histogram):
-        out += idx * val
-    return out
+    return sum(value * count for value, count in enumerate(histogram))
 
 def histogram_mean(image):
     """ Return the mean of the input images’ histogram values. """
-    return histogram_sum(image) / pixel_count(image)
+    return float(histogram_sum(image)) / pixel_count(image)
 
 def histogram_entropy_py(image):
     """ Calculate the entropy of an images' histogram.
@@ -34,12 +47,12 @@ def histogram_entropy_py(image):
             https://git.io/fhqxd
     """
     from math import log2, fsum
-    
-    histogram = image.histogram()
-    histosum = fsum(histogram)
-    histonorm = (histocol / histosum for histocol in histogram)
+    histosum = float(color_count(image))
+    histonorm = (histocol / histosum for histocol in image.histogram())
     
     return -fsum(p * log2(p) for p in histonorm if p != 0.0)
+
+from PIL import Image
 
 histogram_entropy = hasattr(Image.Image, 'entropy') \
                         and Image.Image.entropy \
