87f9be2abdf09e061a111841d9b3abf8c4eed63b
Alexander Böhn <fish2000@gmail.com>
2019-02-25 15:22:37 -0500

    Bump version: 0.6.8 → 0.6.9

--- instakit/__init__.py
+++ instakit/__init__.py
@@ -51,7 +51,7 @@ try:
     exec(compile(open('__version__.py')).read(),
                       '__version__.py', 'exec')
 except:
-    __version__ = '0.6.8'
+    __version__ = '0.6.9'
 
 # module exports:
 __all__ = ('__version__', 'version',
