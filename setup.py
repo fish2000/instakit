05de3b2539ccefd5b2d2f7aa299e689a7bfde417
Alexander Böhn <fish2000@gmail.com>
2019-07-02 01:17:48 -0400

    Bump version: 0.7.3 → 0.7.4

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ try:
             '__version__.py', 'exec'))
 except:
     print("ERROR COMPILING __version__.py")
-    __version__ = '0.7.3'
+    __version__ = '0.7.4'
 
 print("VERSION:", __version__)
 
