a96d0cb2fb5ec758ea1e9e178e851d0f7acd9aba
Alexander Böhn <fish2000@gmail.com>
2020-03-10 02:41:08 -0400

    Bump version: 0.8.9 → 0.8.10

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ try:
             '__version__.py', 'exec'))
 except:
     print("ERROR COMPILING __version__.py")
-    __version__ = '0.8.9'
+    __version__ = '0.8.10'
 
 # PROJECT DESCRIPTION
 LONG_DESCRIPTION = project_content('ABOUT.md')
